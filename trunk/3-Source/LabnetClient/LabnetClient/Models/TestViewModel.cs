95630a2d18211e46c25ac7924c6732260ea56dd0
dvhuyit@gmail.com <dvhuyit@gmail.com@54a1775b-70db-0fe8-46f0-ba19d6e5a36c>
2011-11-05 02:44:14 +0000

    Add TestViewModel

---
+++ trunk/3-Source/LabnetClient/LabnetClient/Models/TestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DomainModel;
+
+namespace LabnetClient.Models
+{
+    public class TestViewModel : BaseModel
+    {
+        public VMTest Test { get; set; }
+        public TestViewModel()
+        {
+            Test = new VMTest(); 
+        }
+    }
+}
