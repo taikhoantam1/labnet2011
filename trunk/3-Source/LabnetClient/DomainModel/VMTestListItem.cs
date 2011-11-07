cfc969feaa3ad85b44662f88b0ae755f32b94a2a
huynp88@gmail.com <huynp88@gmail.com@54a1775b-70db-0fe8-46f0-ba19d6e5a36c>
2011-11-07 16:05:07 +0000

    Add test for partner

---
+++ trunk/3-Source/LabnetClient/DomainModel/VMTestListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModel
+{
+    public class VMTestListItem
+    {
+        public VMTestListItem()
+        {
+            IsDelete = false;
+        }
+        public string TestName { get; set; }
+        public int TesstId { get; set; }
+        public decimal Cost { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
